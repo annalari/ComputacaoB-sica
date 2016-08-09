80f28f8548b3e361e704f64296ed42fe6a10e3cb
xxxxx <xxxx.@gmail.com>
2016-08-09 15:31:28 -0300

    ex4 finished

--- lista1/ex4.c
+++ lista1/ex4.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double atualiza_salario(float salario,float perce_aumento){
-
-
+double faz_tudo(float salario,float perce_aumento){
+ 
+  double aumento = salario*perce_aumento/100;
+  double n_salario = salario+aumento;
+  printf("%.2lf %.2lf",aumento,n_salario);
 
 }
 
-
-
-
-
-
-
-
-
-
 main(){
+ float salario=0;
+ float percent=0;
+ 
+ scanf("%f %f", &salario, &percent);
+ faz_tudo(salario,percent); 
 
-
-
+ 
 }
