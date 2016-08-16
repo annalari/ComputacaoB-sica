04f95ecd3115bdd9de5394f3b61b9d95a4903522
xxxxx <xxxx.@gmail.com>
2016-08-16 14:42:23 -0300

    ex9

---
+++ lista1/ex9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main(){
+ double a,b;
+
+ scanf("%lf %lf", &a, &b );
+ 
+ double power;
+// power = pow(a,b);
+ printf("%.1lf", pow(a,b)); 
+
+
+
+
+
+ return 0;
+}
