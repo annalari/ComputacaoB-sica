e6cfee009b19d2b5fdf5e17a2d0a6380cf6bf124
xxxxx <xxxx.@gmail.com>
2016-08-16 14:52:18 -0300

    ex10

---
+++ lista1/ex10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+ int atual, nascimento;
+
+
+ scanf("%d %d",&nascimento, &atual);
+
+ int idade =  atual-nascimento;
+
+ printf("%d %d",idade,2017-atual+idade);
+
+
+ return 0;
+}
